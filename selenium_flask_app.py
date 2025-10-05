e1141eea2c60fdde3f8b4a30cee68abafaa89201
fluiwork <fluiworkagencia@gmail.com>
2025-10-04 19:55:57 -0500

    adsdasd

--- selenium_flask_app.py
+++ selenium_flask_app.py
@@ -1138,5 +1138,7 @@ def process():
     return jsonify(res)
 
 if __name__ == '__main__':
-    logger.info("Iniciando servidor Flask en http://127.0.0.1:5000")
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
+
