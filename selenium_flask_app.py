bc44f27938aeffffc21b15e162a80a11a6c2371b
fluiwork <fluiworkagencia@gmail.com>
2025-10-06 07:40:38 -0500

    jja

--- selenium_flask_app.py
+++ selenium_flask_app.py
@@ -1423,4 +1423,4 @@ def process():
 
 if __name__ == '__main__':
     # Ejecutar Flask con debug=False para evitar logs de desarrollo
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
